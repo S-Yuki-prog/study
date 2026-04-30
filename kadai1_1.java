f9df4e2bab4a640a17b483f2e2f6144b99a9e7b9
関 優稀 <s2421134@cco.kanagawa-it.ac.jp>
2026-04-30 22:16:31 +0900

    Add files via upload

---
+++ kadai1_1.java
@@ -0,0 +1,16 @@
+package java1_1;
+
+public class kadai1_1 {
+	public static void main( String [] args ) {
+		int idnum = 2421134;
+		String department = " I " ;
+		String name = " Yuki " ;
+		int age = 20 ;
+		
+		System.out.println( " 学籍番号は\t " + idnum + " です。 " ) ;
+		System.out.println( " コースは\t " + department + " 科です。 " ) ;
+		System.out.println( " 名前は\t " + name + " です。 " ) ;
+		System.out.println( " 年齢は\t " + age + " 才です。 " ) ;
+	}
+
+}
